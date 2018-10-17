37cec1cdfc6e27a92e795b989733dff37926672f
Sol Kim <defreturnnone@gmail.com>
2018-10-17 11:16:59 +0900

    separate platform directories

--- psutil/_psutil_sunos.py
+++ psutil/_psutil_sunos.py
@@ -6,7 +6,6 @@ from distutils import sysconfig
 
 module_name = os.path.splitext(os.path.basename(__file__))[0]
 python_version = sysconfig.get_python_version()
-ext = "pyd" if sys.platform == "win32" else "so"
-file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), python_version, "{}.{}".format(module_name, ext)))
+file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), python_version, sys.platform, "{}.so".format(module_name)))
 mod = imp.load_dynamic(module_name, file_path)
 locals().update(mod.__dict__)
